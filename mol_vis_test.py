815840950ce69eaf0bf2952a8fb68f20ad07b0a9
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-06 10:06:28 -0600

    test

---
+++ mol_vis_test.py
@@ -0,0 +1,11 @@
+pip install rdkit-pypi
+import streamlit as st
+from rdkit import Chem
+from rdkit.Chem import Draw
+from PIL import Image
+
+compound_smiles = 'c1cc(C(=O)O)c(OC(=O)C)cc1'
+m = Chem.MolFromSmiles(compound_smiles)
+im=Draw.MolToImage(m)
+
+st.image(im)
