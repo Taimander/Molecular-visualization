1dc368c2d67a184e596af9ad0a35f942ae74cd64
SilkenMocha <102678654+SilkenMocha@users.noreply.github.com>
2022-04-18 21:36:52 -0500

    Update

--- mol_vis_test.py
+++ mol_vis_test.py
@@ -28,21 +28,21 @@ st.title('RDKit + Py3DMOL 😀')
 #st.write('Hello world')
 
 
-#def makeblock(smi):
-    #mol = Chem.MolFromSmiles(smi)
-    #mol = Chem.AddHs(mol)
-    #AllChem.EmbedMolecule(mol)
-    #mblock = Chem.MolToMolBlock(mol)
-    #return mblock
+def makeblock(smi):
+    mol = Chem.MolFromSmiles(smi)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    mblock = Chem.MolToMolBlock(mol)
+    return mblock
 
-#def render_mol(xyz):
-    #xyzview = py3Dmol.view()#(width=1000,height=1000)
-    #xyzview.addModel(xyz,'mol')
-    #xyzview.setStyle({'stick':{}})
-    #xyzview.setBackgroundColor('white')
-    #xyzview.zoomTo()
-    #showmol(xyzview,height=500,width=500)
+def render_mol(xyz):
+    xyzview = py3Dmol.view()#(width=1000,height=1000)
+    xyzview.addModel(xyz,'mol')
+    xyzview.setStyle({'stick':{}})
+    xyzview.setBackgroundColor('white')
+    xyzview.zoomTo()
+    showmol(xyzview,height=500,width=500)
 
-#compound_smiles=st.text_input('SMILES please','CC')
-#blk=makeblock(compound_smiles)
-#render_mol(blk)
+compound_smiles=st.text_input('SMILES please','CC')
+blk=makeblock(compound_smiles)
+render_mol(blk)
